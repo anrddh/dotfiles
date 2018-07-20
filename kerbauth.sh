1ef967ecf0e6465a9d5c3d9de05d781e6502f5fc
Aniruddh Agarwal <aniruddh@Aniruddhs-Old-MacBook-Pro.local>
2018-07-20 16:43:02 -0400

    Remove unused files

--- kerbauth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export PATH="$PATH:/usr/local/bin"
-
-kticket=$(klist | grep UMICH.EDU | grep -v Expired | grep -v Principal)
-
-if [ ! -n "$kticket" ]; then
-     security find-generic-password -a aniruddh -s um_kerb -w | kinit --password-file=STDIN aniruddh && aklog
-fi
